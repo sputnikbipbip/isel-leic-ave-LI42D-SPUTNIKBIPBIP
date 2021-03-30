5981a801bccf81d8402071ca14025b1bbd2ac5ff
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2021-03-30 22:52:38 +0100

    TPC 1 correction

---
+++ TPC 01 - AULA 02/App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PointLib;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point(3, 4);
+            Console.WriteLine("Module = " + p.getModule());
+        }
+    }
+}
