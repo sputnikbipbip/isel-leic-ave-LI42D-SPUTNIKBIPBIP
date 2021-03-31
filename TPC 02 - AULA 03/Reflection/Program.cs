5a69f0f5e54f140971315c16370c180ae4be8468
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2021-03-31 10:58:37 +0100

    TPC 2

---
+++ TPC 02 - AULA 03/Reflection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom("RestSharp.dll");
+            Type[] types = assembly.GetTypes();
+            foreach(Type type in types)
+            {
+                Console.WriteLine("Type -> " + type.Name);
+                MethodInfo[] methods = type.GetMethods();
+                if(methods[0] != null)
+                {
+                    foreach(MethodInfo method in methods)
+                    {
+                        Console.WriteLine("\t Method -> " + method.Name);
+                    }
+                }
+            }
+        }
+    }
+}
